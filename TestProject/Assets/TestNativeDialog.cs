82ff2724ed422c5b7c3f53ac8a293cc49e0a89b9
koki ibukuro <koki.ibukuro@gmail.com>
2013-10-07 00:21:08 +0900

    minor fix

--- TestProject/Assets/TestNativeDialog.cs
+++ TestProject/Assets/TestNativeDialog.cs
@@ -11,13 +11,13 @@ public class TestNativeDialog : MonoBehaviour {
 	}
 
 	void OnGUI () {
-		if(GUILayout.Button("open panel")) {
+		if(GUILayout.Button("open panel", GUILayout.MinHeight(50), GUILayout.MinWidth(100))) {
 			dialog.openPanel((string path) => {
 				Debug.Log("open:"+path);
 			});
 		}
 		
-		if(GUILayout.Button("save panel")) {
+		if(GUILayout.Button("save panel", GUILayout.MinHeight(50), GUILayout.MinWidth(100))) {
 			dialog.savePanel((string path) => {
 				Debug.Log("save:"+path);
 			});
